36330da9f057027b61a445127b42c0a7c05dfc44
Linuka Lolitha Jayesinghe <99358426+it21274398@users.noreply.github.com>
2024-03-28 00:40:20 +0530

    Add the Home and Speech to sinhala text part

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
+export default function Layout() {
+  return (
+    <div className="">
+      <footer>
+        <div className="footer">
+          <h1>All Rights reserved</h1>
+        </div>
+      </footer>
+    </div>
+  );
+}
--- frontend/src/components/HomeScreen.jsx
+++ frontend/src/components/HomeScreen.jsx
@@ -1,40 +1,182 @@
-import React, { useState } from "react";
-import axios from "axios"; // for making HTTP requests
+import React, { useEffect, useState } from "react";
+import "../styles/translate.css";
+import SinglishTranslate from "./singlishTranslate";
+import { swapHorizontalOutline } from "ionicons/icons";
+import { IonIcon } from "@ionic/react";
 
-const Home = () => {
-  const [englishText, setEnglishText] = useState("");
-  const [sinhalaText, setSinhalaText] = useState("");
+function Translate() {
+  const [inputLang, setInputLang] = useState("en");
+  const [outputLang, setOutputLang] = useState("si");
+  const [inputText, setInputText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+  const [singlishInput, setSinglishInput] = useState(""); // Singlish input
+  const [translatedSinglishText, setTranslatedSinglishText] = useState(""); // Singlish translation
+  const [inputCharsCount, setInputCharsCount] = useState(0); // Handle character input limit
 
-  const translateText = async () => {
-    try {
-      const response = await axios.post("/api/translate", {
-        text: englishText,
-      });
-      setSinhalaText(response.data.translatedText);
-    } catch (error) {
-      console.error("Error translating text:", error);
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("darkMode") === "true" || false
+  );
+
+  // Singlish Translation
+  const handleSinglishInputChange = (event) => {
+    const newValue = event.target.value;
+
+    if (newValue.length > 5000) {
+      setSinglishInput(newValue.slice(0, 5000));
+    } else {
+      setSinglishInput(newValue);
     }
+
+    // Perform translation and update translatedText state
+    const newTranslatedText = SinglishTranslate(newValue);
+    setInputCharsCount(newValue.length);
+    setTranslatedSinglishText(newTranslatedText);
   };
 
+  // Translation function
+  const handleTranslation = () => {
+    const inputLanguageCode = inputLang;
+    const outputLanguageCode = outputLang;
+
+    // API URL
+    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguageCode}&tl=${outputLanguageCode}&dt=t&q=${encodeURI(
+      inputText
+    )}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        const translatedText = json[0].map((item) => item[0]).join("");
+        setTranslatedText(translatedText); // Update React state with translated text
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleInputLangChange = (event) => {
+    const selectedLang = event.target.value;
+    setInputLang(selectedLang);
+  };
+
+  const handleOutputLangChange = (event) => {
+    setOutputLang(event.target.value);
+  };
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+
+    if (newValue.length > 5000) {
+      setInputText(newValue.slice(0, 5000));
+    } else {
+      setInputText(newValue);
+    }
+
+    handleTranslation();
+    setInputCharsCount(newValue.length);
+  };
+
+  const handleSwapLanguages = () => {
+    // Swap input and output languages
+    setInputLang((prevInputLang) => (prevInputLang === "en" ? "si" : "en"));
+    setOutputLang((prevOutputLang) => (prevOutputLang === "en" ? "si" : "en"));
+
+    // Swap input and output text values using state variables
+    const tempInputText = inputText;
+    setInputText(translatedText);
+    setTranslatedText(tempInputText);
+  };
+
+  // Set dark mode class on body element
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
+
   return (
-    <div>
-      <h1>English to Sinhala Translator</h1>
-      <div>
-        <textarea
-          value={englishText}
-          onChange={(e) => setEnglishText(e.target.value)}
-          placeholder="Enter English text here"
-          rows={5}
-          cols={50}
-        />
+    <div className="translator-container">
+      <div className="language-selector">
+        <select
+          className="language-dropdown"
+          value={inputLang}
+          onChange={handleInputLangChange}
+        >
+          <option value="en">English</option>
+          <option value="si">සිංහල</option>
+        </select>
+        <div className="swap-position">
+          <IonIcon icon={swapHorizontalOutline} onClick={handleSwapLanguages} />
+        </div>
+        <select
+          className="language-dropdown"
+          value={outputLang}
+          onChange={handleOutputLangChange}
+        >
+          <option value="si" className="option">
+            සිංහල
+          </option>
+          <option value="en" className="option">
+            English
+          </option>
+        </select>
       </div>
-      <button onClick={translateText}>Translate</button>
-      <div>
-        <h2>Sinhala Translation:</h2>
-        <p>{sinhalaText}</p>
+
+      <div className="translation-boxes">
+        {inputLang === "si" && (
+          <div className="input-box">
+            <div className="text-area">
+              <textarea
+                placeholder="Singlish වලින් ලියන්න..."
+                autoFocus
+                value={singlishInput}
+                onChange={handleSinglishInputChange}
+              />
+              <div className="chars">
+                <span id="input-chars">{inputCharsCount}</span> / 5000
+              </div>
+            </div>
+          </div>
+        )}
+
+        <div className="input-box">
+          <div className="text-area">
+            <textarea
+              id="translation-input"
+              placeholder={`Enter text in ${
+                inputLang === "si" ? "Sinhala..." : "English..."
+              }`}
+              value={inputLang === "si" ? translatedSinglishText : inputText}
+              onChange={handleInputChange}
+            />
+            {/* if input language is Sinhala chars will be hidden in normal input box */}
+            {inputLang === "si" ? (
+              <div className="chars" style={{ display: "none" }}>
+                <span id="input-chars">{inputCharsCount}</span> / 5000
+              </div>
+            ) : (
+              <div className="chars">
+                <span id="input-chars">{inputCharsCount}</span> / 5000
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="output-box">
+          <textarea
+            id="translation-output"
+            placeholder={`Translated text in ${
+              outputLang === "si" ? "Sinhala" : "English"
+            }`}
+            value={translatedText}
+            readOnly
+          />
+        </div>
       </div>
     </div>
   );
-};
+}
 
-export default Home;
+export default Translate;
