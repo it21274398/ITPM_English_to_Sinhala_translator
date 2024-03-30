910adaf0931baeeb7868498552f65746fb712ce8
Ashen Dissanayake <101706077+ashenDiz@users.noreply.github.com>
2024-03-30 20:56:54 +0530

    add history component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import HomeScreen from "./components/HomeScreen";
 import UserProfile from "./components/UserProfile";
 import Footer from "./components/Footer";
 import VoiceToText from "./components/VoiceToText";
+import HistoryScreen from "./components/HistoryScreen"
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,8 @@ function App() {
           <Route path="/home" element={<HomeScreen />} />
           <Route path="/Voicetotext" element={<VoiceToText />} />
           <Route path="/user" element={<UserProfile />} />
+          <Route path="/History" element={<HistoryScreen />} />
+
         </Routes>
         <Footer />
       </BrowserRouter>
