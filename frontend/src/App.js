0c0baec7ff81f76c06b351cac2f41983807d1e33
Linuka Lolitha Jayesinghe <99358426+it21274398@users.noreply.github.com>
2024-04-02 11:40:34 +0530

    sdfghjk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import Registerscreen from "./components/RegisterScreen";
 import HomeScreen from "./components/HomeScreen";
 import UserProfile from "./components/UserProfile";
 import AboutUs from "./components/AboutUScreen";
-//sdfghjkl;
 import Hitory from "./components/HitoryScreen";
 import VoiceToText from "./components/VoiceToText";
 import HistoryScreen from "./components/HistoryScreen";
