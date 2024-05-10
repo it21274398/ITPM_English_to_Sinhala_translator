66445daec11d7b1c8c9c61c796b6e404387d2a55
Sandaru Nawoda <it21232572@my.sliit.lk>
2024-05-10 12:52:43 +0530

    user profileadded

---
+++ backend/src/api/routes/userRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+  signup,
+  signin,
+  getAllUsers,
+  getUserProfile,
+} from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+//login singup part
+userRouter.post("/signup", signup);
+userRouter.post("/signin", signin);
+
+//user part
+userRouter.get("/profile", getUserProfile);
+userRouter.get("/all", getAllUsers);
+
+export default userRouter;
